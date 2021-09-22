8467556b8f6ce48a02bd2a47c526c30776c83f1c
Milan <milanvpage@gmail.com>
2021-09-22 15:48:54 -0700

    commit tree 66c4f7436fa52ed565e910fdf92018fc4efdc5fb

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+let drivers = [{ name: "Pax", age: 18 }, { name: "Brody", age: 23}, { name: "Milan", age: 21 }, { name: "Tommy", age: 47 }, { name: "Liz", age: 43 }]
+
+function matchName(drivers, str) {
+    return drivers.filter(driver => driver.name === str);
+
+};
+
+function findMatching(drivers, str){
+    return drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+};
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(driver => driver.startsWith(str));
+};
